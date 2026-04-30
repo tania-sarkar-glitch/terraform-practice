fd4bb6ee9ced23b5418e23ce1054708165a1d3f1
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-04-30 18:24:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,18 @@ resource "local_file" "test" {
   content  = "Hello Terraform from cloud ☁️"
   filename = "test.txt"
 }
+
+variable "filename" {
+  default = "test.txt"
+}
+
+variable "content" {
+  default = "Hello from variable!"
+}
+
+provider "local" {}
+
+resource "local_file" "test" {
+  content  = var.content
+  filename = var.filename
+}
