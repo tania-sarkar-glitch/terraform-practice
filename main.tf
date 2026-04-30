36e604ff56cb4d7f0f98835188098a851611f891
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-04-30 13:17:44 +0000

    updated terraform config

--- main.tf
+++ main.tf
@@ -1,18 +1,25 @@
-variable "filename" {
-  default = "test.txt"
+variable "prefix" {
+  default = "dev"
 }
 
-variable "content" {
-  default = "Hello from variable!"
+variable "files" {
+  default = {
+    "file1.txt" = "Hello from file 1"
+    "file2.txt" = "Hello from file 2"
+    "notes.txt" = "Terraform is fun!"
+    "secret.txt" = "This is hidden power"
+  }
 }
 
 provider "local" {}
 
-resource "local_file" "test" {
-  content  = var.content
-  filename = var.filename
+resource "local_file" "files" {
+  for_each = var.files
+
+  filename = "${var.prefix}-${each.key}"
+  content  = each.value
 }
 
-output "file" {
-  value = var.filename
+output "created_files" {
+  value = [for f in local_file.files : f.filename]
 }
