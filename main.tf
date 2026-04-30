c905f8aab35071d35b292ba83bef3ff569176a88
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-04-30 13:02:43 +0000

    updated terraform config

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+variable "filename" {
+  default = "test.txt"
+}
+
+variable "content" {
+  default = "Hello from variable!"
+}
+
 provider "local" {}
 
 resource "local_file" "test" {
   content  = var.content
   filename = var.filename
+}
+
+output "file" {
+  value = var.filename
 }
