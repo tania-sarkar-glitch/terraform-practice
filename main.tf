5c7603d75a789f24d97d94622c30e379e26adc7a
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-05-07 16:26:29 +0000

    Add output definitions to main.tf

--- main.tf
+++ main.tf
@@ -18,15 +18,20 @@ module "prod_infrastructure" {
   environment = "prod"
 }
 
-# Call for TEST (using the default file_data defined in the module)
-module "prod_infrastructure" {
+# Call for stage (using the default file_data defined in the module)
+module "test_infrastructure" {
   source      = "./modules/welcome_card"
   user_name   = "Tania"
   environment = "test"
 }
 
-output "dev_files" {
-  value = [for f in module.dev_infrastructure : "File created!"]
-  # Note: To output filenames from a module, you'd need a 'module output' 
-  # but let's keep it simple for now.
+# ... (your existing module calls) ...
+
+output "dev_file_location" {
+  # Syntax: module.<NAME>.<OUTPUT_NAME>
+  value = module.dev_infrastructure.card_path
+}
+
+output "prod_file_location" {
+  value = module.prod_infrastructure.card_path
 }
