9a1207e26561ce43623259f737daddbfba851f90
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-05-07 17:31:37 +0000

    new changes

--- main.tf
+++ main.tf
@@ -1,37 +1,55 @@
 provider "local" {}
 
-# Call for DEV
-module "dev_infrastructure" {
-  source      = "./modules/welcome_card"
-  user_name   = "Tania"
-  environment = "dev"
-  file_data   = {
-    "task.txt"   = "Complete the Terraform lab"
-    "status.txt" = "Learning modules"
+locals {
+  environments = {
+    "dev"  = "Tania-Developer"
+    "test" = "Tania-Tester"
+    "prod" = "Tania-Admin"
   }
 }
 
-# Call for PROD (using the default file_data defined in the module)
-module "prod_infrastructure" {
-  source      = "./modules/welcome_card"
-  user_name   = "Tania"
-  environment = "prod"
+module "app_cluster" {
+  source   = "./modules/welcome_card"
+  for_each = local.environments # This is the loop!
+
+  environment = each.key    # takes dev, test, prod
+  user_name   = each.value  # takes the names
 }
 
-# Call for stage (using the default file_data defined in the module)
-module "test_infrastructure" {
-  source      = "./modules/welcome_card"
-  user_name   = "Tania"
-  environment = "test"
+# This tells Terraform to go find this file and read its info
+data "local_file" "outside_file" {
+  filename = "${path.root}/external_secret.txt"
 }
 
-# ... (your existing module calls) ...
+# Now we use that data in a new resource
+resource "local_file" "copied_secret" {
+  filename = "${path.root}/cards/copied_from_data_source.txt"
+  content  = "The secret we read was: ${data.local_file.outside_file.content}"
+}
 
-output "dev_file_location" {
+locals {
+  settings = [
+    { name = "PORT", value = "8080" },
+    { name = "ENV",  value = "production" },
+    { name = "LOG",  value = "debug" }
+  ]
+}
+
+resource "local_file" "app_config" {
+  filename = "${path.root}/app_settings.conf"
+  
+  # This is the "Dynamic" part. It's like a for-loop inside a resource.
+  content = <<EOT
+%{ for s in local.settings ~}
+SET ${s.name}=${s.value}
+%{ endfor ~}
+EOT
+}
+#output "dev_file_location" {
   # Syntax: module.<NAME>.<OUTPUT_NAME>
-  value = module.dev_infrastructure.card_path
-}
+ # value = module.dev_infrastructure.card_path
+#}
 
-output "prod_file_location" {
-  value = module.prod_infrastructure.card_path
-}
+#output "prod_file_location" {
+  #value = module.prod_infrastructure.card_path
+#}
--- modules/welcome_card/main.tf
+++ modules/welcome_card/main.tf
@@ -1,7 +1,10 @@
 resource "local_file" "welcome_files" {
   for_each = var.file_data
-
-  # This creates files like: cards/dev-Tania-welcome.txt
   filename = "${path.root}/cards/${var.environment}-${var.user_name}-${each.key}"
-  content  = "${each.value}\nEnvironment: ${var.environment}\nUser: ${var.user_name}"
+  content  = "${each.value}"
+
+  lifecycle {
+    # If you try to delete this file via Terraform, it will fail!
+    prevent_destroy = true 
+  }
 }
